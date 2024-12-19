6d8e8bad94b09f09259c8f5a06ef93f4ab641a33
Helga Souza <91810665+helguiita2021@users.noreply.github.com>
2024-12-19 19:38:18 -0300

    Primeiro Commit - Criando Estrutura git status

---
+++ Converso_de_Unidades.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+//Instrutura do MENU do Código
+
+int main( ){
+    int opcao;
+
+
+    while(1){ // MENU INTERATIVO
+    printf("BEM VINDO AO PROGRAMA DE CONVERSÃO DE UNIDADES - EMBARCATECH:\n");
+    printf("Escolha qual conversão gostaria de fazer:\n");
+    printf(" Digite 1 - Unidades de Comprimento\n");
+    printf(" Digite 2 - Unidades de Massa\n");
+    printf(" Digite 3 - Unidades de Volume\n");
+    printf(" Digite 4 - Unidades de Temperatura\n");
+    printf(" Digite 5 - Unidades de Velocidade\n");
+    printf(" Digite 6 - Unidades de Energia\n");
+    printf(" Digite 7 - Unidades de Área\n");
+    printf(" Digite 8 - Unidades de Tempo\n");
+    printf(" Digite 9 - Unidades de Dados\n");
+    printf(" Digite 10 - Caso deseje Sair do Sistema\n");
+    printf(" Informe a sua escolha:\n");
+    scanf("%d",&opcao);
+    
+    switch(opcao){
+
+
+
+
+
+
+
+
+
+
+    }
+
+    }
+       
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Converso_de_Unidades
+
+
+Instrução de como usar o  Programa:
+
+
